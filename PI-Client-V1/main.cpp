94e3ecb9cc53957899c76b87f96c19e47bcaa3bb
Matthias Van Gestel <maregt0@gmail.com>
2013-02-27 01:19:55 +0100

    Add Socketand SPI information in comment

--- PI-Client-V1/main.cpp
+++ PI-Client-V1/main.cpp
@@ -3,8 +3,12 @@
 /* Socket:
  *  http://www.youtube.com/watch?v=u5OdR46542M
  *  http://qt-project.org/doc/qt-4.8/qtcpsocket.html
- *
- *
+ *  http://qt.developpez.com/doc/4.6/network-programming/
+ *  http://www.qtforum.org/article/9/qt-network-tutorial.html
+ * SPI:
+ *  http://www.haifux.org/lectures/258/gpio_spi_i2c_userspace.pdf
+ *  http://armbedded.eu/node/318
+ *  http://www.raspberrypi.org/phpBB3/viewtopic.php?p=132128
  */
 
 int main(int argc, char *argv[])
