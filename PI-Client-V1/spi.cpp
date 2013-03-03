7d448873d9982ab0455ddc555a940435c227be10
Matthias Van Gestel <maregt0@gmail.com>
2013-03-03 20:02:41 +0100

    Add some more SPI code

--- PI-Client-V1/spi.cpp
+++ PI-Client-V1/spi.cpp
@@ -1,3 +1,6 @@
+/* Based on https://github.com/WiringPi/WiringPi/blob/master/wiringPi/wiringPiSPI.c
+ *
+ */
 #include "spi.h"
 
 #include <stdio.h>
@@ -13,33 +16,48 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 
+// --------------
+static char       *spiDev0 = "/dev/spidev0.0" ;
+static char       *spiDev1 = "/dev/spidev0.1" ;
+static __u8     spiMode   = 0 ;
+static __u16     spiBPW    = 8 ;
+static __u16    spiDelay  = 0;
+
+static __u32    spiSpeeds [2] ;
+static int         spiFds [2] ;
+
+
 // https://www.kernel.org/doc/Documentation/spi/spidev
 // https://www.kernel.org/doc/Documentation/spi/spidev_fdx.c
 // https://github.com/WiringPi/WiringPi/blob/master/wiringPi/wiringPiSPI.c
 
 SPI::SPI(){
-    int spidev = open("/dev/spidev0.1", O_RDWR);
-    dumpstat("/dev/spidev0.1",spidev);
+    dumpstat("/dev/spidev0.1");
+    setupSPI();
+    dumpstat("/dev/spidev0.1");
 }
 
-void SPI::dumpstat(const char *name, int fd)
-{
+void SPI::dumpstat(const char *name){
+
+    //channel &= 1;
+    //fd = dev;
+
     __u8	mode, lsb, bits;
     __u32	speed;
 
-    if (ioctl(fd, SPI_IOC_RD_MODE, &mode) < 0) {
+    if (ioctl(dev, SPI_IOC_RD_MODE, &mode) < 0) {
         perror("SPI rd_mode");
         return;
     }
-    if (ioctl(fd, SPI_IOC_RD_LSB_FIRST, &lsb) < 0) {
+    if (ioctl(dev, SPI_IOC_RD_LSB_FIRST, &lsb) < 0) {
         perror("SPI rd_lsb_fist");
         return;
     }
-    if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits) < 0) {
+    if (ioctl(dev, SPI_IOC_RD_BITS_PER_WORD, &bits) < 0) {
         perror("SPI bits_per_word");
         return;
     }
-    if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed) < 0) {
+    if (ioctl(dev, SPI_IOC_RD_MAX_SPEED_HZ, &speed) < 0) {
         perror("SPI max_speed_hz");
         return;
     }
@@ -50,18 +68,34 @@ void SPI::dumpstat(const char *name, int fd)
 
 int SPI::setSPImode(void){
     struct spi_ioc_transfer spi;
-    channel &= 1 ;
+    //channel &= 1 ;
 
-     spi.tx_buf        = (unsigned long)data ;
-     spi.rx_buf        = (unsigned long)data ;
-     spi.len           = len ;
-     spi.delay_usecs   = spiDelay ;
-     spi.speed_hz      = spiSpeeds [channel] ;
-     spi.bits_per_word = spiBPW ;
+     //spi.tx_buf        = (unsigned long)data ;
+     //spi.rx_buf        = (unsigned long)data ;
+     //spi.len           = len ;
+     //spi.delay_usecs   = spiDelay ;
+     //spi.speed_hz      = spiSpeeds [channel] ;
+     //spi.bits_per_word = spiBPW ;
 
-     return ioctl (spiFds [channel], SPI_IOC_MESSAGE(1), &spi) ;
+     //return ioctl (spiFds [channel], SPI_IOC_MESSAGE(1), &spi) ;
+    return 0;
 }
 
-#define spiDelay 0
-#define spiBPW 8
-#define spiDelay 0
+void SPI::setupSPI(int channel, int speed){
+    //
+
+    //
+    channel &= 1;
+
+    dev = open(channel == 0 ? spiDev0 : spiDev1, O_RDWR);
+
+    spiSpeeds[channel] = speed;
+
+    ioctl(dev, SPI_IOC_WR_MODE, &spiMode);
+    ioctl(dev, SPI_IOC_RD_MODE, &spiMode);
+    ioctl(dev, SPI_IOC_WR_BITS_PER_WORD, &spiBPW);
+    ioctl(dev, SPI_IOC_RD_BITS_PER_WORD, &spiBPW);
+    ioctl(dev, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    ioctl(dev, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+}
+
--- PI-Client-V1/spi.h
+++ PI-Client-V1/spi.h
@@ -1,12 +1,16 @@
 #ifndef SPI_H
 #define SPI_H
 
+#include <linux/types.h>
+
 class SPI
 {
+    int dev;
 public:
     SPI();
-    void dumpstat(const char *name, int fd);
+    void dumpstat(const char *name);
     int setSPImode(void);
+    void setupSPI(int channel = 0, int speed = 500000);
 };
 
 #endif // SPI_H
