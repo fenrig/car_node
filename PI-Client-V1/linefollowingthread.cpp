6f3380149d89ca17b749367e7d11675fd44e690e
degoossez <driesgoossens93@gmail.com>
2013-04-04 14:04:32 +0200

    debugging

--- PI-Client-V1/linefollowingthread.cpp
+++ PI-Client-V1/linefollowingthread.cpp
@@ -15,9 +15,9 @@ void linefollowingthread::run(){
     offsets os;
     UartCamera cam;
     cam.changeCompression(255);
-    std::vector<char> data = cam.GetPicture();
     for(int i=0;i<100;i++)
     {
+        std::vector<char> data = cam.GetPicture();
         os = fl.FindOffset(data,i);
         int size = 2;
         __u8 msg[size];
