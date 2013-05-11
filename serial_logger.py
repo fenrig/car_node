8874a50ae152743f55e31f34cbe7c173b8a1a653
Matthias Van Gestel <maregt0@gmail.com>
2013-05-11 14:44:17 +0200

    add python logger

---
+++ serial_logger.py
@@ -0,0 +1,22 @@
+import signal
+import sys
+import serial
+
+
+def signal_handler(signal, frame):
+    print("ctrl+c")
+    fileobject.write("\nend\n")
+    fileobject.close()
+    sys.exit()
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+fileobject = open('serial_logger.txt', 'a+')
+
+signal.signal(signal.SIGINT, signal_handler)
+
+while(True):
+    line = ser.readline()
+    print(line)
+    fileobject.write(line)
+
+fileobject.close()
